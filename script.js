4eb23cd4244faa89d464b79e8f00222d3a5cfccb
Ailsadm <ailsamcgowan@hotmail.co.uk>
2023-01-14 21:12:10 +0000

    questions and answers showing

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let choices = document.querySelector("#choices");
 let timer = document.querySelector("#time");
 let questionTitle = document.querySelector("#question-title");
 let startScreenContainer = document.querySelector("#start-screen")
-let questionsE1 = document.querySelector("#questions");
+let questionsContainer = document.querySelector("#questions");
 
 let questionsArray = [{question: "exQ1", answers: ["Ex1", "Ex2", "Ex3"], correctAnswer: 2},
 {question: "exQ2", answers: ["Ex1", "Ex2", "Ex3"], correctAnswer: 2},
@@ -11,44 +11,59 @@ let questionsArray = [{question: "exQ1", answers: ["Ex1", "Ex2", "Ex3"], correct
 ]
 
 let currentQuestion = 0;
+let currentChoices = 0;
 
 startButton.addEventListener("click", function (event) {
-   let timerCount = 90;
-
-   let intervalID = setInterval(function () {
-        timerCount--
-        timer.textContent = timerCount
-        if(timerCount === 0) {
-            clearInterval(intervalID);}
+  let timerCount = 90;
+  
+  let intervalID = setInterval(function () {
+    timerCount--
+    timer.textContent = timerCount
+    if(timerCount === 0) {
+      clearInterval(intervalID);}
     }, 1000)
-
+    
     startScreenContainer.classList.add("hide")
-    questionsE1.classList.remove("hide")
-
+    questionsContainer.classList.remove("hide")
+    
     eachquestion()
+    choicesSelection()
+  })
+  
+  
+function eachquestion(event) {
+  // event.preventDefault()
+  // const question = questionsArray[currentQuestion];
+  for (let i = 0; i < questionsArray.length; i++) {
+    // const question = questionsArray[i].question;
+    currentQuestion = questionsArray[i].question
+  }
+  questionTitle.innerText = currentQuestion
+}
+// if (currentQuestion < questionsArray.length - 1) {
+//     currentQuestion++;
+//     eachquestion();
+//   } else {
+//     alert("You've reached the last question.");
+//   }
+// }
+
+
+choices.addEventListener("click", function(event) {
+  
 })
 
-function eachquestion() {
-    const question = questionsArray[currentQuestion];
-    for (let i = 0; i < questionsArray.length; i++) {
-        // const question = questionsArray[i];
-        currentQuestion = questionsArray[i]
-}
-if (currentQuestion < questionsArray.length - 1) {
-    currentQuestion++;
-    eachquestion();
-  } else {
-    alert("You've reached the last question.");
+function choicesSelection(){
+
+  for (let i = 0; i < questionsArray.length; i++) {
+    // const question = questionsArray[i].question;
+    currentChoices = questionsArray[i].answers
   }
+  choices.innerText = currentChoices
+
 }
 
 
-
-
-// choices.addEventListener("click", function(event) {
-
-//     if (event.target.matches("button")) {
-//         event.target.getAttribute("data-index")
-//     }
-// })
-
+    // if (event.target.matches("button")) {
+    //     event.target.getAttribute("data-index")
+    // }
