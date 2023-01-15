f02be180e4aecea313e4347242ca4626e83a60e6
Ailsadm <ailsamcgowan@hotmail.co.uk>
2023-01-15 13:37:17 +0000

    choice button

--- script.js
+++ script.js
@@ -4,17 +4,22 @@ let timer = document.querySelector("#time");
 let questionTitle = document.querySelector("#question-title");
 let startScreenContainer = document.querySelector("#start-screen")
 let questionsContainer = document.querySelector("#questions");
+let answer1 = document.querySelector("#answer1");
+let answer2 = document.querySelector("#answer2");
+let answer3 = document.querySelector("#answer3");
+let endscreen = document.querySelector("#end-screen")
+let finalscore = document.querySelector("#final-score")
 
-let questionsArray = [{question: "exQ1", answers: ["Ex1", "Ex2", "Ex3"], correctAnswer: 2},
-{question: "exQ2", answers: ["Ex1", "Ex2", "Ex3"], correctAnswer: 2},
-{question: "exQ3", answers: ["Ex1", "Ex2", "Ex3"], correctAnswer: 2}
+let questionsArray = [{question: "What does JS stand for", answers: ["JavaScript", "JunctionStart", "JavaStart"], correctAnswer: 0},
+{question: "exQ2", answers: ["Ex1-1", "Ex2", "Ex3"], correctAnswer: 2},
+{question: "exQ3", answers: ["Ex1-2", "Ex2", "Ex3"], correctAnswer: 2}
 ]
 
-let currentQuestion = 0;
+let currentQuestion;
 let currentChoices = 0;
+let timerCount = 90;
 
 startButton.addEventListener("click", function (event) {
-  let timerCount = 90;
   
   let intervalID = setInterval(function () {
     timerCount--
@@ -26,44 +31,43 @@ startButton.addEventListener("click", function (event) {
     startScreenContainer.classList.add("hide")
     questionsContainer.classList.remove("hide")
     
-    eachquestion()
-    choicesSelection()
+    setQuestionandanswers()
+   
   })
   
+let questionCount = 0;
+function setQuestionandanswers(event) {
+  if (questionCount <= questionsArray.length - 1){
+    currentQuestion = questionsArray[questionCount].question
   
-function eachquestion(event) {
-  // event.preventDefault()
-  // const question = questionsArray[currentQuestion];
-  for (let i = 0; i < questionsArray.length; i++) {
-    // const question = questionsArray[i].question;
-    currentQuestion = questionsArray[i].question
-  }
   questionTitle.innerText = currentQuestion
+
+  currentChoices = questionsArray[questionCount].answers
+
+  answer1.textContent = currentChoices[0];
+  answer2.textContent = currentChoices[1];
+  answer3.textContent = currentChoices[2];
+  }
+  else{
+    clearInterval(timer)
+    finalscore.textContent = timerCount
+    endscreen.classList.remove("hide")
+    questionsContainer.classList.add("hide")
+  }
 }
-// if (currentQuestion < questionsArray.length - 1) {
-//     currentQuestion++;
-//     eachquestion();
-//   } else {
-//     alert("You've reached the last question.");
-//   }
-// }
 
 
-choices.addEventListener("click", function(event) {
-  
+answer1.addEventListener("click", function(event) {
+  questionCount++
+  // questionsArray[questionCount].correctAnswer
+  setQuestionandanswers(event.target.textContent)
+
+  if (questionsArray[0].answers === questionsArray[0].correctAnswer) {
+    return true
+  }
+  else {
+    return false;
+    timerCount -10;
+  }
 })
 
-function choicesSelection(){
-
-  for (let i = 0; i < questionsArray.length; i++) {
-    // const question = questionsArray[i].question;
-    currentChoices = questionsArray[i].answers
-  }
-  choices.innerText = currentChoices
-
-}
-
-
-    // if (event.target.matches("button")) {
-    //     event.target.getAttribute("data-index")
-    // }
